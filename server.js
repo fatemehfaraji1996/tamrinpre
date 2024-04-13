1344d0d42fb497881a3c6db7cf6934d821bad771
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-04-13 11:55:45 +0330

    add midelver

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express();
-app.listen(5000,()=>{
-    console.log('server is runing on port http://locklhast:5000');
+app.get('/',(req,res)=>{
+res.json({ masage :'hello'})
+})
+app.listen(3000,()=>{
+    console.clear();
+    console.log('server is runing on port http://localhost:3000');
 })
